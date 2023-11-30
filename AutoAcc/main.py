f69c8240c618be52a8bf5735ef4d2433144b0f0d
David/Dave Thomsen <71229951+dthomsen116@users.noreply.github.com>
2023-11-30 06:11:54 -0800

    edit main

--- AutoAcc/main.py
+++ AutoAcc/main.py
@@ -3,25 +3,31 @@ import urllib3
 
 urllib3.disable_warnings()
 
-def readCreds(filename):
-    try:
-        with open(filename, 'r') as file:
-            creds = {}
-            for line in file:
-                key, value = line.strip().split('=')
-                creds[key.strip()] = value.strip()
-            return creds
-    except Exception as e:
-        print(f"Error reading credentials: {e}")
-        return None
+class ProxmoxConnection():
+    def __init__(self, creds_filename):
+        # Initialize the class with credentials and SmartConnect instance
+        self.creds = self.readCreds(creds_filename)
+        self.si = None
 
-def connection():
-    creds = readCreds('.\\creds.txt')
-    if creds is None:
-        return
-    proxmox = ProxmoxAPI("192.168.0.160:8006", user=creds.get('user'), password=creds.get('passwd'), verify_ssl=False
-                        )
-    #print(proxmox.nodes.get())
+    def readCreds(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                creds = {}
+                for line in file:
+                    key, value = line.strip().split('=')
+                    creds[key.strip()] = value.strip()
+                return creds
+        except Exception as e:
+            print(f"Error reading credentials: {e}")
+            return None
 
+    def connection(self):
+        creds = self.readCreds('.\\creds.txt')
+        if creds is None:
+            return
+        proxmox = ProxmoxAPI("192.168.0.160:8006", user=creds.get('user'), password=creds.get('passwd'), verify_ssl=False)
+        #print(proxmox.nodes.get())
 
-connection()
+# Create an instance of the class and call the connection method
+proxmox_connection = ProxmoxConnection('.\\creds.txt')
+proxmox_connection.connection()
