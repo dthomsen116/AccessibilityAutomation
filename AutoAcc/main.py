a6618df7d82ef0237eff4b1998060c344ca0f36a
dthomsen116 <71229951+dthomsen116@users.noreply.github.com>
2023-12-05 12:48:53 -0500

    List Nodes

--- AutoAcc/main.py
+++ AutoAcc/main.py
@@ -3,11 +3,11 @@ import urllib3
 
 urllib3.disable_warnings()
 
-class ProxmoxConnection():
-    def __init__(self, creds_filename):
-        # Initialize the class with credentials and SmartConnect instance
+class ProxmoxManager:
+    def __init__(self, host, creds_filename):
+        # Initialize the class with credentials and ProxmoxAPI instance
         self.creds = self.readCreds(creds_filename)
-        self.si = None
+        self.proxmox = ProxmoxAPI(host, user=self.creds.get('user'), password=self.creds.get('passwd'), verify_ssl=False)
 
     def readCreds(self, filename):
         try:
@@ -21,13 +21,52 @@ class ProxmoxConnection():
             print(f"Error reading credentials: {e}")
             return None
 
-    def connection(self):
-        creds = self.readCreds('.\\creds.txt')
-        if creds is None:
-            return
-        proxmox = ProxmoxAPI("192.168.0.160:8006", user=creds.get('user'), password=creds.get('passwd'), verify_ssl=False)
-        #print(proxmox.nodes.get())
+    def checkNode(self):
+        try:
+            # Get the list of nodes
+            nodes = self.proxmox("nodes").get()
 
-# Create an instance of the class and call the connection method
-proxmox_connection = ProxmoxConnection('.\\creds.txt')
-proxmox_connection.connection()
+            print("Connected Nodes:")
+            for node in nodes:
+                print(f"- {node['node']}:")
+
+                # Get the list of Containers on the node
+                containers = self.proxmox(f"nodes/{node['node']}/lxc").get()
+                for container in containers:
+                    print(f"\t{container['vmid']}. {container['name']} => {container['status']}")
+
+                # Get the list of VMs on the node
+                vms = self.proxmox(f"nodes/{node['node']}/qemu").get()
+                #print("Virtual Machines on {0}:".format(node['node']))  # Debugging line
+                # print(vms)  # Debugging line
+                for vm in vms:
+                    vname = (f"\t{vm['vmid']}. {vm['name']} => {vm['status']}")
+                    print(vname)
+        except Exception as e:
+            print(f"Error checking nodes: {e}")
+
+
+    def displayMenu(self):
+        print("1. Look at the ProxyBoxy!")  # Add your specific operations here
+        print("2. Exit")
+
+    def mainMenu(self):
+        while True:
+            self.displayMenu()
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                # Add your Proxmox operations here
+                self.checkNode()
+            elif choice == '2':
+                print("Exiting program. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    creds_filename = 'AutoAcc\creds.txt'
+    host = "192.168.0.160:8006"
+    
+    manager = ProxmoxManager(host, creds_filename)
+    manager.mainMenu()
