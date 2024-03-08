a6d8c7827914c6791ed264e395ebebaae3c5a12b
dthomsen116 <david.thomsen@mymail.champlain.edu>
2024-03-08 15:40:06 -0500

    creds?

---
+++ Capstone-Utils/CSVs/CollectSheets.py
@@ -0,0 +1,8 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+with open()
